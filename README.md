a7bd790533574eb9855fd4458fbc67c320a67cd8
Henry Arnold <devharnold@gmail.com>
2024-06-11 09:22:56 +0300

    works correctly

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Pm-C
 C repo for learning concepts of programming methodology in school. Re-learning C
+
+Satrts with a test to ensure files are working as expected
---
+++ test-file.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * Test program to ensure my environment is working
+ * Start with a simple "Hello, World" message
+ * Returns 0 upon success else return 1
+ */
+
+int main(){
+	printf("Hello, Wold!");
+	return 0;
+}
