6b0606e436bdd9106d333527faef809e82ee6c85
Henry Arnold <devharnold@gmail.com>
2024-07-02 14:04:45 +0300

    Code: added for loop program in C

---
+++ prog_meth/repetition.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+	/*for loop syntax
+	for (initialization; test_expression; update_variable) {
+		//code to execute
+	}*/
+	int i;
+
+	//program to print integers 1 to 10 using for loop
+	for (i = 1; i <= 10; i++) {
+		printf("The value of i is: %d \n", i);
+	}
+
+	return 0;
+}
